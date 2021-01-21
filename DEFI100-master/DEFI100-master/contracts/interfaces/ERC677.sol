98c69339e3a0074d2425693b4d7dd35d653e774d
Wrapp3d <74978916+wrapp3d@users.noreply.github.com>
2021-01-21 23:40:59 +0530

    Delete DEFI100-master/DEFI100-master directory

--- DEFI100-master/DEFI100-master/contracts/interfaces/ERC677.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.4.24;
-
-
-contract ERC677 {
-    function transfer(address to, uint256 value) public returns (bool);
-    function transferAndCall(address to, uint value, bytes data) public returns (bool success);
-
-    event Transfer(address indexed from, address indexed to, uint value, bytes data);
-}
